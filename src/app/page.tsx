bb5092ba4b6fcd2f0d4ab69d9aa14c7ec39b5e0b
100431310 <100431310@alumnos.uc3m.es>
2023-10-01 23:39:36 +0200

    Version control advance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,40 @@
 import { Button } from "@/components/ui/button";
-import { UserButton, auth } from "@clerk/nextjs";
+import { UserButton,auth } from "@clerk/nextjs";
+import { Heading1 } from "lucide-react";
 import Link from "next/link";
-import { ArrowRight, LogIn } from "lucide-react";
-import FileUpload from "@/components/FileUpload";
-import { checkSubscription } from "@/lib/subscription";
-import SubscriptionButton from "@/components/SubscriptionButton";
-import { db } from "@/lib/db";
-import { chats } from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
+import {LogIn} from 'lucide-react'
 
 export default async function Home() {
-  const { userId } = await auth();
-  const isAuth = !!userId;
-  const isPro = await checkSubscription();
-  let firstChat;
-  if (userId) {
-    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
-    if (firstChat) {
-      firstChat = firstChat[0];
-    }
-  }
+  const {userId} = await auth()
+  const isAuth = !!userId
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-3 text-5xl font-semibold">Chatea con tu PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
-
           <div className="flex mt-2">
-            {isAuth && firstChat && (
-              <>
-                <Link href={`/chat/${firstChat.id}`}>
-                  <Button>
-                    Go to Chats <ArrowRight className="ml-2" />
-                  </Button>
-                </Link>
-                <div className="ml-3">
-                  <SubscriptionButton isPro={isPro} />
-                </div>
-              </>
-            )}
+            {isAuth && <Button>Ir al Chat</Button>}
           </div>
-
-          <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Join millions of students, researchers and professinals to instantly
-            anwer questions and understand research with AI
+          <p className="max-w-xl mt-1 text-lg">
+            Únete a millones de estudiantes, investigadores y 
+            profesionales para responder a tus preguntas con IA.
           </p>
-
           <div className="w-full mt-4">
-            {isAuth ? (
-              <FileUpload />
-            ) : (
+            {isAuth ? (<h1>fileupload</h1>):(
               <Link href="/sign-in">
                 <Button>
-                  Login to get Started!
+                  ¡Inicia sesión para empezar!
                   <LogIn className="w-4 h-4 ml-2" />
                 </Button>
               </Link>
             )}
           </div>
+
         </div>
       </div>
     </div>
-  );
+    )
 }
