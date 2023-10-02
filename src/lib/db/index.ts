178e5e796b9b2c8465a4213e107e7172a6710e6a
chachestriki <jd.oficial111@gmail.com>
2023-10-02 16:26:48 +0200

    DB configuration

---
+++ src/lib/db/index.ts
@@ -0,0 +1,13 @@
+import {neon, neonConfig} from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+
+neonConfig.fetchConnectionCache = true;
+
+import {} from 'drizzle-orm/neon-http'
+if (!process.env.DATABASE_URL) {
+    throw new Error('database url not found');
+}
+
+const sql = neon(process.env.DATABASE_URL);
+
+export const db = drizzle(sql);
