eece68de9977f56ceac4364fc71bd530c76ea281
chachestriki <jd.oficial111@gmail.com>
2023-10-05 09:56:40 +0200

    AWS v2

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 
-export async functinon uploadToS3(file:File) {
+export async function uploadToS3(file:File) {
     try{
         AWS.config.update({
             accessKeyId: process.env.AWS_ACCESS_KEY_ID,
