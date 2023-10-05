15620a2a5f04295afcea842f05debd514ca96694
chachestriki <jd.oficial111@gmail.com>
2023-10-05 14:18:53 +0200

    AWS v3

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -3,8 +3,25 @@ import AWS from 'aws-sdk';
 export async function uploadToS3(file:File) {
     try{
         AWS.config.update({
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
+        });
+        const s3 = new AWS.S3({
+            params: {
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+            },
+            region: 'eu-west-3'
         })
-    }   catch (error)
-};
+        
+        const file_key = 'uploads/' + Date.now().toString() + file.name.replace(' ','-')
+
+        const params = {
+            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+            Key: file.name,
+            Body: file
+        }
+
+        const upload = s3.putObject(params)
+
+    }   catch (error){}
+}
